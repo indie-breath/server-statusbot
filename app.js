a224127bdfe94933e1e158fbc7f32eecf8933fa5
indiebreath <indiebreath@proton.me>
2024-12-23 08:48:58 +1100

    Output added to file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import * as compose from "docker-compose";
 import path from "node:path";
+import * as fs from "node:fs";
 import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,6 +8,8 @@ const __dirname = path.dirname(__filename);
 
 const result = await compose.ps({ cwd: path.join(__dirname) });
 var output = "";
+var oldOutput;
+
 result.data.services.forEach((service) => {
     if (service.name == "web") {
         output = service.state;
@@ -14,8 +17,14 @@ result.data.services.forEach((service) => {
 });
 
 if (output.length == 0) {
-    console.log("Server is down.");
+    fs.writeFile("status.txt", "Server is down.", function (err) {
+        if (err) throw err;
+    });
+    oldOutput = "Server is down.";
 }
 else if (output.length > 0) {
-    console.log("Server is up.");
+    fs.writeFile("status.txt", "Server is up.", function (err) {
+        if (err) throw err;
+    });
+    oldOutput = "Server is down.";
 }
