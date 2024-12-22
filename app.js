690396e0c059aa990b616f4a87eba21e7e3ca4ac
indiebreath <indiebreath@proton.me>
2024-12-23 08:53:16 +1100

    Made everything loop

--- app.js
+++ app.js
@@ -6,25 +6,29 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const result = await compose.ps({ cwd: path.join(__dirname) });
-var output = "";
-var oldOutput;
+setInterval(async function() {
+    const result = await compose.ps({ cwd: path.join(__dirname) });
+    var output = "";
+    var oldOutput;
 
-result.data.services.forEach((service) => {
-    if (service.name == "web") {
-        output = service.state;
+    result.data.services.forEach((service) => {
+        if (service.name == "web") {
+            output = service.state;
+        }
+    });
+
+    if (output.length == 0) {
+        fs.writeFile("status.txt", "Server is down.", function (err) {
+            if (err) throw err;
+        });
+        oldOutput = "Server is down.";
     }
-});
+    else if (output.length > 0) {
+        fs.writeFile("status.txt", "Server is up.", function (err) {
+            if (err) throw err;
+        });
+        oldOutput = "Server is down.";
+    }
 
-if (output.length == 0) {
-    fs.writeFile("status.txt", "Server is down.", function (err) {
-        if (err) throw err;
-    });
-    oldOutput = "Server is down.";
-}
-else if (output.length > 0) {
-    fs.writeFile("status.txt", "Server is up.", function (err) {
-        if (err) throw err;
-    });
-    oldOutput = "Server is down.";
-}
+    console.log("Ran everything.");
+}, 5 * 1000);
