16d4cd766db3ee70e4293af35a0783df3bea07b8
indiebreath <indiebreath@proton.me>
2024-12-23 08:36:00 +1100

    Added up/down checking for specific service

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-import { ps } from "docker-compose";
+import * as compose from "docker-compose";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const result = await ps({ cwd: path.join(__dirname) });
+const result = await compose.ps({ cwd: path.join(__dirname) });
+var output = "";
 result.data.services.forEach((service) => {
-    console.log(service.state);    
+    if (service.name == "web") {
+        output = service.state;
+    }
 });
+
+if (output.length == 0) {
+    console.log("Server is down.");
+}
+else if (output.length > 0) {
+    console.log("Server is up.");
+}
